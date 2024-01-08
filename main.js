8cab68628c2078b0256584e55fc07df45a3d9e06
Jorge Rosas Windows <abraham.work7@gmail.com>
2024-01-07 19:36:21 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const digitalClock = () => {
+     //get the corrent time
+     let now = new Date();
+     let hours = now.getHours();
+     let minutes = now.getMinutes();
+     let seconds = now.getSeconds();
+     let AmOrPm = hours >= 12 ? 'pn' : 'am';
+     
+     hours = (hours % 12) || 12;
+
+     hours = (hours < 10) ? '0' + hours : hours;
+     minutes = (minutes < 10) ? '0' + minutes : minutes;
+     seconds = (seconds < 10) ? '0' + seconds : seconds;   
+
+
+
+     document.querySelector('.hours').textContent = hours + ':';
+     document.querySelector('.minutes').textContent = minutes + ':';
+     document.querySelector('.seconds').textContent = seconds;
+     document.querySelector('.amorpm').textContent = AmOrPm;
+}
+
+
+setInterval(digitalClock, 1000);
